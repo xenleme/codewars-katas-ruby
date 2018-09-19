db150d07163b46dcc2244d74ce8500603c4448ac
xenleme <xenleme@gmail.com>
2018-09-19 11:17:36 +0300

    Add task description to 6 kyu - Is a number prime?

--- 6kyu/is_a_number_prime.rb
+++ 6kyu/is_a_number_prime.rb
@@ -1,3 +1,13 @@
+# Define a function isPrime/is_prime() that takes one integer argument and
+# returns true/True or false/False depending on if the integer is a prime.
+#
+# Per Wikipedia, a prime number (or a prime) is a natural number greater than 1
+# that has no positive divisors other than 1 and itself.
+#
+# Assumptions:
+# You can assume you will be given an integer input.
+# You can not assume that the integer will be only positive. You may be given negative numbers as well (or 0)
+
 def is_prime(num)
   return false if num < 2
 
