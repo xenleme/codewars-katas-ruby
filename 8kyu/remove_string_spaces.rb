cf46f0b0ac020a203c6f02641c151104c2126a30
xenleme <xenleme@gmail.com>
2018-11-04 14:43:04 +0300

    Add solution to: 8 kuy - Remove String Spaces

---
+++ 8kyu/remove_string_spaces.rb
@@ -0,0 +1,8 @@
+# Simple, remove the spaces from the string, then return the resultant string.
+
+def no_space(x)
+  x.gsub(' ', '')
+end
+
+no_space('8 j 8   mBliB8g  imjB8B8  jl  B')
+no_space('8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd')
