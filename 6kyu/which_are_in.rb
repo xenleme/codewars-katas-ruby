358f107a4155bb62cd34ae232c096358c2cd9087
xenleme <xenleme@gmail.com>
2018-11-11 20:11:17 +0300

    Add solution to: 6 kuy - Which are in?

---
+++ 6kyu/which_are_in.rb
@@ -0,0 +1,33 @@
+# Given two arrays of strings a1 and a2 return a sorted array r in lexicographical
+# order of the strings of a1 which are substrings of strings of a2.
+#
+# Example 1:
+# a1 = ["arp", "live", "strong"]
+# a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
+# returns ["arp", "live", "strong"]
+#
+# Example 2:
+# a1 = ["tarp", "mice", "bull"]
+# a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
+# returns []
+#
+# Notes:
+# - Arrays are written in "general" notation. See "Your Test Cases" for examples
+# in your language.
+#
+# - In Shell bash a1 and a2 are strings. The return is a string where words are separated by commas.
+#
+# - Beware: r must be without duplicates.
+#
+# - Don't mutate the inputs.
+
+def in_array(array1, array2)
+  array1.select { |s| array2.any? { |w| w.include?(s) } }.sort.uniq
+end
+
+a1 = ["arp", "live", "strong"]
+a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
+a3 = ["tarp", "mice", "bull"]
+
+puts in_array(a1, a2)
+puts in_array(a3, a2)
